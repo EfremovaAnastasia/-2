3152af2240a1fae485040b028f354b0d3b158e9f
EfremovaAnastasia <nastaefremova116@gmail.com>
2025-11-13 09:22:21 +0300

    Update лабораторная.java

--- код_1/лабораторная.java
+++ код_1/лабораторная.java
@@ -1,1 +1,21 @@
-
+import java.util.Scanner;
+public class лабораторная {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int k = 0;
+        int n = in.nextInt();
+        for (int i = 1; i < n + 1; i++) {
+            if (i % 2 != 0)
+                System.out.println((int)Math.pow(2, i));
+            else
+                System.out.println(f(i));
+        }
+    }
+    public static long f(int x) {
+        long a = 1;
+        for (int i = 2; i <= x; i++){
+            a *= i;
+        }
+        return a;
+    }
+}
